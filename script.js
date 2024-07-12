a8f2c72367827bcc267760494f3d257b663cf50e
TizianoReynaga <reynaga.tiziano@tecnica7.edu.ar>
2024-07-12 19:45:20 -0300

    Buscador

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('generarPeli').addEventListener('click')
+const apiKey = '66504332'
+const NombrePeli = document.getElementById('nomPeli')
+
+
+function peliRand(){
+    const result = fetch("http://www.omdbapi.com/?apikey=&")
+
+    console.log(`${}`)
+}
+
+
+
+
